75060d99c95473f6613c7efadde8174aa5050100
Ilian Bosserz <iil.bossserez@gmail.com>
2024-11-22 14:51:42 +0100

    ik heb een check gedaan of je admin role bent voordat je in admin dashboard kan. Als je geen admin bent --> zie je dat je geen toegang hebt  en wordt je geredirect naar login

--- admin-dash.php
+++ admin-dash.php
@@ -6,6 +6,16 @@ include_once(__DIR__ . '/classes/Admin.php');
 include_once(__DIR__ . '/classes/Category.php');
 session_start();
 
+if ($_SESSION['role'] !== 1) {
+    // redirect naar log in
+
+    echo '<h1  style="text-align: center;
+    padding:10%">' . htmlspecialchars('You do not have access to this page') . '</h1>';
+    //wacht 3 seconden en redirect naar login
+    header("refresh:3;url=login.php");
+    exit();
+}
+
 
 ?>
 
@@ -14,6 +24,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Admin Dashboard</title>
 </head>
 <body>
@@ -22,7 +33,7 @@ session_start();
     <h2>Add Category</h2>
     <form method="post" action="">
         <label for="category_name">Category Name:</label>
-        <input type="text" id="category_name" name="category_name" required>
+        <input type="text" id="category_name" name="category_name" required pattern="[A-Za-z0-9\s]+" title="Only letters, numbers, and spaces are allowed">
         <br>
         <label for="category_picture">Category Picture:</label>
         <input type="file" id="category_image" name="category_image" required>
@@ -33,7 +44,7 @@ session_start();
     <h2>Add Product</h2>
     <form method="post" action="">
         <label for="product_name">Product Name:</label>
-        <input type="text" id="product_name" name="product_name" required>
+        <input type="text" id="product_name" name="product_name" required pattern="[A-Za-z0-9\s]+" title="Only letters, numbers, and spaces are allowed">
         <br>
         <label for="product_price">Product Price:</label>
         <input type="number" id="product_price" name="product_price" required>
