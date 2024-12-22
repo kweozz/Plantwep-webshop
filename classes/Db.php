3d5e2e735fb1f9e9e503a696f3bab35b87a99b8b
kweozz <r0771028@student.thomasmore.be>
2024-12-22 15:43:17 +0100

    feat: add Composer autoload files and dotenv dependency

--- classes/Db.php
+++ classes/Db.php
@@ -1,20 +1,27 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv as Dotenv;
+
 class Db {
     // Declare the static property $conn
     private static $conn;
 
     // Method to get the database connection
     public static function getConnection() {
+        // Load environment variables
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+
         // Check if the connection already exists
         if (self::$conn === null) {
             try {
-                // Use Railway database connection details
-                $host = $_ENV['MYSQLHOST'] ?? 'autorack.proxy.rlwy.net';
-                $port = $_ENV['MYSQLPORT'] ?? '45276';
-                $dbname = $_ENV['MYSQLDATABASE'] ?? 'railway';
-                $username = $_ENV['MYSQLUSER'] ?? 'root';
-                $password = $_ENV['MYSQLPASSWORD'] ?? 'wRKVCAXctmaZqxdZXcqWcyxQeBUdvsjV';
-                
+                // Use Railway database connection details from environment variables
+                $host = $_ENV['MYSQLHOST'];
+                $port = $_ENV['MYSQLPORT'];
+                $dbname = $_ENV['MYSQLDATABASE'];
+                $username = $_ENV['MYSQLUSER'];
+                $password = $_ENV['MYSQLPASSWORD'];
 
                 // Create a new connection
                 self::$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
