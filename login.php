d151be13f27295a63bc5aa3c3f2d2099525f8fb9
kweozz <r0771028@student.thomasmore.be>
2024-12-22 16:42:48 +0100

    fix: correct case in include statement for User class in login.php

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 session_start(); // Start the session only once at the top
-include_once __DIR__ . '/classes/User.php';
+include_once __DIR__ . '/classes/user.php';
+
 include_once __DIR__ . '/classes/Db.php';
 
 $error = ''; // Variable to hold error message
