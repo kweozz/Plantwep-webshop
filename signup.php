f8d65e825908db1b061816fc7cf2d532dc2580c2
kweozz <r0771028@student.thomasmore.be>
2024-12-26 15:17:04 +0100

    style: replace form.css with new login.css for improved styling and layout

--- signup.php
+++ signup.php
@@ -49,7 +49,7 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup - Plantwerp</title>
-    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
